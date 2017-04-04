89a5a40dfe278eb456a2b395a2564f0851913673
Evan Berkowitz <git@evanberkowitz.com>
2017-04-04 22:19:08 +0200

    Degeneracy Table.

--- subduce.py
+++ subduce.py
@@ -2,36 +2,16 @@ from __future__ import print_function
 import os, sys
 import argparse
 import numpy
-import irreps
 import octahedral
 import n_squared
 
-nsq = n_squared.vectors
-
 print( octahedral.group )
 
-# print("nsquared table")
-# [ print(n,len(nsq(n)), [ len(octahedral.group.on(i)) for i in nsq(n) ] ,nsq(n)) for n in range(20) ]
+print("Degeneracy table")
 
-# v=octahedral.group.on([0,0,1])
-# print("Image of [0,0,1] under O_H:", v)
-#
-# c=octahedral.group.classes[1]
-# print(c)
-# print(nsq(3))
-# print(octahedral.group.on(nsq(3)[0]))
-# print(c.character_nsq(octahedral.group.on(nsq(3)[0])))
-# print(octahedral.group.nsq_degeneracy("A1g",nsq(0)[0]))
+irreps=["A1g", "A2g", "T1g", "T2g", "Eg", "A1u", "A2u", "T1u", "T2u", "Eu"]
+row_format = "{:>5}{:>12}{:>12}" + "{:>5}" * len(irreps)
+print(row_format.format("n^2", "degeneracy", "vector", *irreps))
+[ print(row_format.format(n, len(octahedral.group.on(vec)), vec, *[ (lambda d: "" if d==0 else d)(numpy.sum(numpy.array(octahedral.group.nsq_degeneracy(irrep,vec).astype(int)))) for irrep in irreps ])) for n in range(24) if len(n_squared.vectors(n)) is not 0 for vec in n_squared.vectors(n) ]
 
-# def test(ns, irrep):
-#     return octahedral.group.nsq_degeneracy(irrep,nsq(ns)[0])
-#
-# [ print(n, test(n,"Eu")) for n in range(7) ]
-# [ print(irrep, test(14,irrep)) for irrep in octahedral.group.irreps.keys() ]
 
-print(octahedral.group.classes[1].ops[0])
-print(octahedral.group.classes[1].ops[0].representation(octahedral.group.on(nsq(3)[0])))
-
-P = octahedral.group.nsq_projector("Eg",nsq(1)[0])
-
-print(P)
